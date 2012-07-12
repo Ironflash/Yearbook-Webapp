031065a6d6f0ecb56d031fa80afd115952b8db46
Gregory <gregorylaflash@gmail.com>
2012-07-12 17:42:13 -0500

    Created User

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ YearbookWebapp::Application.routes.draw do
 
   root to: 'static_pages#home'
 
+  match '/signup', to:'users#new'
+
   match '/help', to:'static_pages#help'
 
   match '/about', to:'static_pages#about'
