1a44c120d07f4e97e7f9e3f5cac8401588c8f6be
Gregory <gregorylaflash@gmail.com>
2012-07-12 17:26:38 -0500

    Finished re-routing and basic links

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,75 +2,47 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  subject {page}
+
   describe "Home page" do
 
-    it "should have the h1 'Texas A&M Yearbook'" do
-      visit home_path
-      page.should have_selector('h1', :text=> "Texas A&M Yearbook")
-    end
+    before { visit root_path }
 
-    it "should have the base title" do
-    	visit home_path
-    	page.should have_selector('title', :text=>"Texas A&M Yearbook")
-    end
+    it { should have_selector('h1', text: "Texas A&M Yearbook") }
 
-    it "should not have a custom title" do
-      visit home_path
-      page.should_not have_selector('title', :text=>" | Home")
-    end
+    it { should have_selector('title', text: full_title('')) }
+
+    it { should_not have_selector 'title', text: '| Home' }
+
   end
 
   describe "Help page" do
 
-  	it "should have the h1 'Help'" do 
-  		visit help_path
-  		page.should have_selector('h1', :text=>"Help")
-  	end
+    before { visit help_path }
 
-  	it "should have the base title" do
-  		visit help_path
-  		page.should have_selector('title', :text=> "Texas A&M Yearbook")
-  	end
+  	it { should have_selector('h1', text:"Help") }
 
-    it "should not have a custom title" do
-      visit help_path
-      page.should_not have_selector('title', :text=>" | Help")
-    end
+  	it { should have_selector('title', text: full_title('Help')) }
+
   end
 
   describe "About Page" do
 
-  	it "should have the h1 'About Us'" do
-  		visit about_path
-  		page.should have_selector('h1', :text=>"About Us")
-  	end
+    before { visit about_path }
 
-  	it "should have the base title" do 
-  		visit about_path
-  		page.should have_selector('title', :text=>"Texas A&M Yearbook")
-  	end
+  	it { should have_selector('h1', text:"About Us") }
 
-    it "should not have a custom title" do
-      visit about_path
-      page.should_not have_selector('title', :text=>" | About Us")
-    end
+  	it { should have_selector('title', text: full_title('About Us')) }
+
   end
 
   describe "Contact Page" do
 
-  	it "should have the h1 'Contact Us'" do
-  		visit contact_page
-  		page.should have_selector('h1', :text=>"Contact Us")
-  	end
+    before { visit contact_path }
 
-  	it "should have the base title" do
-  		visit contact_page
-  		page.should have_selector('title', :text=>"Texas A&M Yearbook")
-  	end
+  	it { should have_selector('h1', text:"Contact Us") }
 
-    it "should not have a custom title" do
-      visit contact_page
-      page.should_not have_selector('title', :text=>" | Contact")
-    end
+  	it { should have_selector('title', text: full_title('Contact')) }
+
   end
 end
