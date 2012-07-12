78dde3540ea133508766e5d7f04ae930232846f5
Gregory <gregorylaflash@gmail.com>
2012-07-12 14:01:29 -0500

    Added full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+	#returns the full page title per individual page
+	def full_title(page_title)
+		base_title = "Texas A&M Yearbook"
+		if page_title.empty?
+			base_title
+		else
+			"#{base_title} | #{page_title}"
+		end
+	end
 end
