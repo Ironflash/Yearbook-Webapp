4b57c6217d5b662d46bb0c5a105e72755da38bfd
Gregory <gregorylaflash@gmail.com>
2012-07-13 12:16:50 -0500

    Make a basic user model

---
+++ app/models/user.rb
@@ -0,0 +1,26 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer         not null, primary key
+#  name       :string(255)
+#  email      :string(255)
+#  phone      :string(255)
+#  position   :string(255)
+#  created_at :datetime        not null
+#  updated_at :datetime        not null
+#
+
+class User < ActiveRecord::Base
+  attr_accessible :email, :name, :phone, :position, :password, :password_confirmation
+  has_secure_password
+
+  before_save { |user| user.email = email.downcase }
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :name , presence: true, length:{ maximum: 50 }
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  validates :position, presence: true
+  validates :password, presence: true, length:{ minimum: 6 }
+  validates :password_confirmation, presence: true
+end
---
+++ db/migrate/20120712225448_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :phone
+      t.string :position
+
+      t.timestamps
+    end
+  end
+end
