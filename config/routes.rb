910fe5aae804e27d3ddc4800d2d13f87792ddeb8
Gregory <gregorylaflash@gmail.com>
2012-07-13 16:37:06 -0500

    Finish member creation

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 YearbookWebapp::Application.routes.draw do
-  get "users/new"
+  resources :users
 
   root to: 'static_pages#home'
 
---
+++ spec/factories.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+	factory :user do 
+		name "Gregory LaFlash"
+		email "grl342@neo.tamu.edu"
+		phone "817-313-6659"
+		position "Admin"
+		password "foobar"
+		password_confirmation "foobar"
+	end
+end
