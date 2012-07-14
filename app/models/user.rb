0638d6bad49ffa9f535ec34a345dadb6d11071be
Gregory <gregorylaflash@gmail.com>
2012-07-14 07:16:37 -0500

    Finish sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,6 +16,7 @@ class User < ActiveRecord::Base
   has_secure_password
 
   before_save { |user| user.email = email.downcase }
+  before_save :create_remember_token
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :name , presence: true, length:{ maximum: 50 }
@@ -23,4 +24,10 @@ class User < ActiveRecord::Base
   validates :position, presence: true
   validates :password, presence: true, length:{ minimum: 6 }
   validates :password_confirmation, presence: true
+
+  private
+
+    def create_remember_token
+      self.remember_token = SecureRandom.urlsafe_base64
+    end
 end
